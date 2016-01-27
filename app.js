7c69823250a0b6a41ce8473d7669973be1ab7a0c
Óscar Vázquez <oscarvb94@gmail.com>
2016-01-27 18:23:33 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
-// Include dependencies
-var express = require("express");
-var fse = require('fs-extra');
-var http = require("http");
-var mongoose = require('mongoose');
-var multer = require('multer');
-var qs = require('querystring');
+// app.js
 
-var app = express();
-var server = http.createServer(app);
+// Incluímos las dependencias que vamos a utilizar
+var express = require("express"),
+    app     = express(),
+    http    = require("http"),
+    qs = require('querystring'),
+    multer = require('multer'),
+    fse = require('fs-extra'),
+    mongoose = require('mongoose'),
+    server  = http.createServer(app);
+
 var upload = multer({ dest: 'public/' });
 
 // Configuramos la app para que pueda realizar métodos REST
 app.configure(function () {
-	app.use(app.router);
-	app.use(express.methodOverride()); // HTTP: PUT and DELETE support
-	app.use(express.limit('50M'));    // Max file size
+
+  app.use(express.methodOverride()); // HTTP PUT and DELETE support
+  app.use(express.limit('20mb'));    // Tamaño maximo
+  app.use(app.router); 		     			 // simple route management
+
 });
 
 // petición GET para obtener una canción
@@ -72,7 +76,7 @@ app.post('/', upload.fields([{ name: 'image', maxCount: 1 }, { name: 'track', ma
 	res.send(200);
 })
 
-// Server listening on port 3000
+// El servidor escucha en el puerto 3000
 server.listen(3000, function() {
-	console.log("Node server running on :3000");
+  console.log("Node server running on :3000");
 });
