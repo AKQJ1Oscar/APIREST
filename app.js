e68a65b32df1305c46096a8404a5f9feb99c709e
Óscar Vázquez <oscarvb94@gmail.com>
2016-01-27 19:15:58 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Include dependencies
+// Dependencies
 var express = require("express");
 var fse = require('fs-extra');
 var http = require("http");
@@ -11,12 +11,38 @@ app.use(app.router);
 app.use(express.methodOverride());
 app.use(express.limit('50mb'));
 
-var server = http.createServer(app);
-server.listen(3000, function() {
+http.createServer(app).listen(3000, function() {
 	console.log("NodeJS server running on :3000");
 });
 
 var upload = multer({ dest: 'uploads/' });
+
+// petición POST para subir una canción
+app.post('/', upload.fields([{ name: 'image', maxCount: 1 }, { name: 'track', maxCount: 1 }]), function (req, res, next) {
+  console.log('Datos de la canción subida: ' + req.files['track'][0]);
+  var cancion = req.files['track'][0];
+  //mover la canción de directorio a los nas
+  fse.move(cancion.path, '/mnt/nas/canciones/' + cancion.originalname, function (err) {
+   	if (err) return console.error(err);
+  	console.log("success!")
+  });
+	//comprobación de si existe imagen
+  if (req.files['image'] !== undefined) {
+	  console.log('Datos de la portada subida: ' + req.files['image'][0]);
+	  var imagen = req.files['image'][0];
+		//copia la imagen de forma síncrona a los nas		
+		try {
+			fse.copySync(imagen.path, '/mnt/nas/imagenes/' + imagen.originalname);
+		} catch (err) {
+			console.error('Oh no, there was an error: ' + err.message)
+		}
+		fse.unlink(imagen.path, function(err){
+			if (err) return console.error(err);
+			console.log('delete success');
+		});
+	}
+	res.send(200);
+})
 
 // petición GET para obtener una canción
 app.get('/cancion/:trackname', function(req, res) {
@@ -45,30 +71,3 @@ app.delete('/imagen/:imagename', function(req, res) {
   });
   res.send(200);
 });
-
-// petición POST para subir una canción
-app.post('/', upload.fields([{ name: 'image', maxCount: 1 }, { name: 'track', maxCount: 1 }]), function (req, res, next) {
-  console.log('Datos de la canción subida: ' + req.files['track'][0]);
-  var cancion = req.files['track'][0];
-  //mover la canción de directorio a los nas
-  fse.move(cancion.path, '/mnt/nas/canciones/' + cancion.originalname, function (err) {
-   	if (err) return console.error(err);
-  	console.log("success!")
-  });
-	//comprobación de si existe imagen
-  if (req.files['image'] !== undefined) {
-	  console.log('Datos de la portada subida: ' + req.files['image'][0]);
-	  var imagen = req.files['image'][0];
-		//copia la imagen de forma síncrona a los nas		
-		try {
-			fse.copySync(imagen.path, '/mnt/nas/imagenes/' + imagen.originalname);
-		} catch (err) {
-			console.error('Oh no, there was an error: ' + err.message)
-		}
-		fse.unlink(imagen.path, function(err){
-			if (err) return console.error(err);
-			console.log('delete success');
-		});
-	}
-	res.send(200);
-})
