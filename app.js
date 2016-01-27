19ead9335ca22d6e5c395b21a8937e5d6a3686bd
Óscar Vázquez <oscarvb94@gmail.com>
2016-01-27 20:10:08 +0100

    Update app.js

--- app.js
+++ app.js
@@ -15,57 +15,49 @@ var qs = require('querystring');
 app.post('/', multer({ dest: 'uploads/' }).fields([{ name: 'image', maxCount: 1 }, { name: 'track', maxCount: 1 }]), function (req, res, next) {
 	console.log('INFO: A track is being uploaded');
   	var cancion = req.files['track'][0];
-  	// mover la canción de directorio a los nas
+  	// Move track to nas directory
   	fse.move(cancion.path, '/mnt/nas/canciones/' + cancion.originalname, function (err) {
    		if (err) return console.error('ERROR: ' + err);
-  		console.log("OK: Track uploaded successfully")
+  		console.log('OK: Track uploaded successfully')
 	});
 	// Upload cover if exists
 	if (req.files['image'] !== undefined) {
 		console.log('INFO: A cover for the track is being uploaded');
 		var imagen = req.files['image'][0];
-		// copia la imagen de forma síncrona a los nas		
-//		try {
-//			fse.copySync(imagen.path, '/mnt/nas/imagenes/' + imagen.originalname);
-//		} catch (err) {
-//			console.error('ERROR: ' + err)
-//		}
+		// Move cover to nas directory
 		fse.move(imagen.path, '/mnt/nas/imagenes/' + imagen.originalname, function (err) {
 			if (err) return console.error('ERROR: ' + err);
 			console.log('OK: Cover uploaded successfully');
 		});
-//		fse.unlink(imagen.path, function (err) {
-//			if (err) return console.error('ERROR: ' + err);
-//			console.log('OKiloko: Cover uploaded successfully');
-//		});
 	}
 	res.send(200);
-})
+});
 
-// petición GET para obtener una canción
+// GET request - Get track
 app.get('/cancion/:trackname', function(req, res) {
-  res.sendfile('/mnt/nas/canciones/' + req.params.trackname);
+	res.sendfile('/mnt/nas/canciones/' + req.params.trackname);
 });
 
-// petición GET para obtener una imagen
-app.get('/imagen/:imagename', function(req, res) {
-  res.sendfile('/mnt/nas/imagenes/' + req.params.imagename);
-});
 
-// petición DELETE para borrar una canción
+// DELETE request - Delete track
 app.delete('/cancion/:trackname', function(req, res) {
-  fse.unlink('/mnt/nas/canciones/' + req.params.trackname, function(err){
-		if (err) return console.error(err);
-		console.log('delete success');
-  });
-  res.send(200);
+	fse.unlink('/mnt/nas/canciones/' + req.params.trackname, function(err){
+		if (err) return console.error('ERROR: ' + err);
+		console.log('OK: Track deleted successfully');
+	});
+	res.send(200);
 });
 
-// petición DELETE para borrar una imagen
+// GET request - Get cover
+app.get('/imagen/:imagename', function(req, res) {
+	res.sendfile('/mnt/nas/imagenes/' + req.params.imagename);
+});
+
+// // DELETE request - Delete cover
 app.delete('/imagen/:imagename', function(req, res) {
-  fse.unlink('/mnt/nas/imagenes/' + req.params.imagename, function(err){
-		if (err) return console.error(err);
-		console.log('delete success');
-  });
-  res.send(200);
+	fse.unlink('/mnt/nas/imagenes/' + req.params.imagename, function(err){
+		if (err) return console.error('ERROR: ' + err);
+		console.log('OK: Cover uploaded successfully');
+	});
+	res.send(200);
 });
